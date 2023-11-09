fab004576db600fd812324bebd956faba03c30ae
SHIVA-6699 <pillashivakrishna6@gmail.com>
2023-11-10 01:50:38 +0530

    S3 Images Fetching

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifystroagereact": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifystroagereact950e42fc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "contactstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Storage } from "aws-amplify";
 
-function App() {
+const YourComponent = () => {
+  const [imageUrls, setImageUrls] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getdata = async () => {
+    try {
+      const { results } = await Storage.list("real/", { pageSize: "ALL" });
+      console.log("Results:", results); // Check the results in the console
+      if (results.length > 0) {
+        const urls = await Promise.all(
+          results.map(async (result) => {
+            return await Storage.get(result.key);
+          })
+        );
+        console.log("Image URLs:", urls); // Check the image URLs in the console
+        setImageUrls(urls);
+      } else {
+        console.log("No files found.");
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading && <p>Loading images...</p>}
+      {!loading &&
+        imageUrls.map((imageUrl, index) => (
+          <img key={index} src={imageUrl} alt={`Image ${index + 1}`} />
+        ))}
     </div>
   );
-}
+};
 
-export default App;
+export default YourComponent;
